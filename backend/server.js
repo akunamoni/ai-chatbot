c34405c4ffc942e5b16f82720d744cc1dda22b8f
vamshiinnings2 <vamshi.kunamoni@innings2.com>
2025-11-06 11:29:34 +0530

    updated the server.js file

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://aichat-bot-rust.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth',authRoutes);
