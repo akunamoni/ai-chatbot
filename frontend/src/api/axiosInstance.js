d777af55e718140368c89abc3fa80a6a608200a9
vamshiinnings2 <vamshi.kunamoni@innings2.com>
2025-11-06 11:38:54 +0530

    updated

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://chatbotai-chi-rouge.vercel.app",
+  baseURL: "https://chatbotai-chi-rouge.vercel.app/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
